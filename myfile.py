8ab2e621a8169e7261d0d92c2da8a32e1e52e148
sheersha2004-UCLA <sheersha2004@gmail.com>
2025-10-01 19:13:51 -0700

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = input("Which course are you taking? ")
+    if x == "ECE 180":
+        print(x + " is the best class ever")
+    else:
+        print(x + " is also great!")
+    
+    print("Bruins are the best!")
